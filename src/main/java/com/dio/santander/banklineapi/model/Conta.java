da4e846a8b94d3e25b669ad19f1f46faa76bb207
Mtbal <104974372+Mtbal@users.noreply.github.com>
2022-05-05 18:14:36 -0300

    mapeando as minhas entidades com JPA

--- src/main/java/com/dio/santander/banklineapi/model/Conta.java
+++ src/main/java/com/dio/santander/banklineapi/model/Conta.java
@@ -1,13 +1,20 @@
 package com.dio.santander.banklineapi.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Conta {
-	private Long numero;
+	@Column(name = "conta_número")
+	private Long número;
+	
+	@Column(name = "conta_saldo")
 	private Double saldo;
 	public Long getNumero() {
-		return numero;
+		return número;
 	}
-	public void setNumero(Long numero) {
-		this.numero = numero;
+	public void setNumero(Long número) {
+		this.número = número;
 	}
 	public Double getSaldo() {
 		return saldo;
